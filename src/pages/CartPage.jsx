22cd9b886138ffb19c0c3828da1e2d2caa7e5c0a
JeanPiton <jean.orpiton@gmail.com>
2023-07-13 11:07:41 -0300

    Fix: cart page inicailization

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,37 +1,12 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import CartProduct from "../components/CartProduct"
 import { PageBody, ProductCardInfo, ProductsCard, ProductsCardContent, ProductsCardHeader} from "../style/CartBody"
 import { useNavigate } from "react-router-dom"
+import { UserContext } from "../contexts/userContext"
+import axios from "axios"
 
 const CartPage = () => {
-
-    const test2=[
-        {
-            "id": "64af03de73f8e151ebd7db8e",
-            "title": "produto4",
-            "description": "descrição do produto4",
-            "price": 70.5,
-            "stock": 5,
-            "imgUrl": "https://img.freepik.com/fotos-gratis/imagem-aproximada-da-cabeca-de-um-lindo-leao_181624-35855.jpg?w=2000"
-        },
-        {
-            "id": "64af03de73f8e151ebd7db8f",
-            "title": "produto5",
-            "description": "descrição do produto5",
-            "price": 69.5,
-            "stock": 4,
-            "imgUrl": "https://img.freepik.com/fotos-gratis/imagem-aproximada-da-cabeca-de-um-lindo-leao_181624-35855.jpg?w=2000"
-        },
-        {
-            "id": "64af03de73f8e151ebd7db8e",
-            "title": "produto4",
-            "description": "descrição do produto4",
-            "price": 70.5,
-            "stock": 5,
-            "imgUrl": "https://img.freepik.com/fotos-gratis/imagem-aproximada-da-cabeca-de-um-lindo-leao_181624-35855.jpg?w=2000"
-        }
-    ]
-    
+    const {config} = useContext(UserContext)    
     const [prods,setProds] = useState()
     const [total,setTotal] = useState()
     const navigate = useNavigate()
@@ -39,11 +14,11 @@ const CartPage = () => {
     useEffect(()=>{
         const fetchData = async () => {
             try{
-              const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/cart`);
+              const {data} = await axios.get(`${import.meta.env.VITE_API_URL}/cart`,config);
               const l = createList(data)
               setProds(l)
             } catch ({response: {status, statusText, data}}){
-              console.log(`${status} ${statusText}\n${data}`);
+                alert(`${status} ${statusText}\n${data}`);
             }
         }
         fetchData();
@@ -86,7 +61,7 @@ const CartPage = () => {
                     Produtos
                 </ProductsCardHeader>
                 {
-                    !prods||prods.length==0?<button onClick={()=>navigate("/produtos")}>Procurar produtos</button>
+                    !prods||prods.length==0?<button onClick={()=>navigate("/")}>Procurar produtos</button>
                     :prods.map(element => <CartProduct key={element._id} prod={element} deleteP={deleteProd} changeQ={changeQtd}/>)
                 }
             </ProductsCard>
